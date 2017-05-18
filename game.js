04a2afcf02312c4ce0fb1b10b22e3674fc75aba0
ravikishorethella <ravikishorethella@gmail.com>
2017-05-18 18:45:56 -0400

    removed hardcoded color

--- game.js
+++ game.js
@@ -11,13 +11,16 @@ var colors = [
 var squares = document.querySelectorAll(".square");
 
 // for time being we are hard coding the target color
-var pickedColor = colors[2];
+var pickedColor = pickColor();
 
 //
 var colorDisplay = document.getElementById("colorDisplay");
 
 // the colorDisplay should be changed to the picked color
 colorDisplay.textContent = pickedColor;
+
+// selecting the message
+var messageDisplay = document.querySelector("#message");
 
 // iterating over all the squares
 for(var i = 0; i < squares.length; i++){
@@ -32,9 +35,27 @@ for(var i = 0; i < squares.length; i++){
         // chech the condition to see if the clickedColor matches with the pickedColor
         if(clickedColor === pickedColor){
             alert("correct");
+            messageDisplay.textContent = "Correct!";
+            changeColors(clickedColor);
         }else{
-            alert("wrong");
+            // if the pickedcolor is not matched then we need to fade the color out
+            // this is nothing bu tsetting the color to the original background color
+            this.style.backgroundColor = "#232323";
+            messageDisplay.textContent = "Try Again";
         }
     });
 } 
 
+function changeColors(color){
+    // we need to loop through the squares
+    for(var i=0; i<squares.length; i++){
+        squares[i].style.backgroundColor = color;
+    }
+}  
+
+// for picking the random color
+function pickColor(){
+    // to generate a random number in js we use the math.random()
+    var random = Math.floor(Math.random() * colors.length);
+    return colors[random];
+}
