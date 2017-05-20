e01693ec0523bbdefcca42ce083df2392c4dd66f
ravikishorethella <ravikishorethella@gmail.com>
2017-05-19 22:10:06 -0400

    random 6 colors added

--- game.js
+++ game.js
@@ -1,11 +1,5 @@
-var colors = [
-    "rgb(255, 0, 0)",
-    "rgb(255, 255, 0)",
-    "rgb(0, 255, 0)",
-    "rgb(0, 255, 255)",
-    "rgb(0, 0, 255)",
-    "rgb(255, 0, 255)"
-]
+// instead of hard coding the color values we will generate the colors randomly  
+var colors = generateRandomColors(6);
 
 // selecting all the squares 
 var squares = document.querySelectorAll(".square");
@@ -15,6 +9,9 @@ var pickedColor = pickColor();
 
 //
 var colorDisplay = document.getElementById("colorDisplay");
+
+// targeting the h1 tag
+var h1 = document.querySelector("h1"); 
 
 // the colorDisplay should be changed to the picked color
 colorDisplay.textContent = pickedColor;
@@ -37,6 +34,7 @@ for(var i = 0; i < squares.length; i++){
             alert("correct");
             messageDisplay.textContent = "Correct!";
             changeColors(clickedColor);
+            h1.style.backgroundColor = clickedColor;
         }else{
             // if the pickedcolor is not matched then we need to fade the color out
             // this is nothing bu tsetting the color to the original background color
@@ -58,4 +56,27 @@ function pickColor(){
     // to generate a random number in js we use the math.random()
     var random = Math.floor(Math.random() * colors.length);
     return colors[random];
+}
+
+// code for generating the random colors
+function generateRandomColors(num){
+    // create an array
+    var arr = [];
+    //loop through the number
+    for(var i=0; i<num; i++){
+        // need to get the random color and push that into arr
+        arr.push(randomColor());
+    }
+    return arr;
+}
+
+function randomColor(){
+    // SELECT one color of red from 0- 255
+    var r = Math.floor(Math.random() * 256);
+    // SELECT one color of green from 0- 255
+    var g = Math.floor(Math.random() * 256);
+    // SELECT one color of blue from 0- 255
+    var b = Math.floor(Math.random() * 256);
+    // return the color
+    return "rgb(" + r + ", " + g + ", " + b +")";    
 }
